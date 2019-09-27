092e412577795837ccefb805548db8e906565e7c
Mx. Kas Perch <the@nodebotani.st>
2019-09-27 14:02:38 -0700

    Fixed

--- final/node-sentimentbot/index.js
+++ final/node-sentimentbot/index.js
@@ -1,4 +1,6 @@
-const tmi = require('tmi.js');
+const tmi = require('tmi.js')
+const superagent = require('superagent')
+require('dotenv').config()
 
 // Define configuration options
 const opts = {
@@ -26,14 +28,22 @@ async function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
 
   // Remove whitespace from chat message
-  const message = msg.trim()
-
-  await fetch('https://twitchcon.csharpfritz.com/sentiment/', {
-    method: 'POST',
-    body: {
+  const message = msg.trim();
+  let response = superagent.post('https://twitchcon.csharpfritz.com/sentiment')
+    .send({
       SentimentText: message
-    }
-  })
+    })
+    .end((err, res) => {
+      console.log(err)
+    })
+
+  console.log(response)
+}
+
+// Function called when the "dice" command is issued
+function rollDice () {
+  const sides = 20;
+  return Math.floor(Math.random() * sides) + 1;
 }
 
 // Called every time the bot connects to Twitch chat
